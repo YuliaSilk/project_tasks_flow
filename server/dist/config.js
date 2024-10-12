1bf256183f5f2d49621c029993e8250f19ddafcb
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-10-12 22:58:05 +0300

    initial commit

---
+++ server/dist/config.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = exports.DB_HOST = void 0;
+exports.DB_HOST = process.env.DB_HOST;
+exports.PORT = process.env.PORT || 3001;
